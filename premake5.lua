1cef0d0b72b7251a0b4735e794ba340379868c1b
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-03-02 19:35:54 +0900

    스크립트의 라인 수를 축소

--- premake5.lua
+++ premake5.lua
@@ -8,7 +8,6 @@ solution "CSharpProgramming"
     -- 솔루션 경로를 설정합니다.
     location "%{wks.name}"
 
-    -- 01.HelloWorld 프로젝트입니다.
     project "01.HelloWorld"
         -- 프로젝트의 종류를 설정합니다.
         kind "ConsoleApp"
@@ -17,14 +16,10 @@ solution "CSharpProgramming"
         language "C#"
 
         -- 소스 코드 경로를 추가합니다.
-        includedirs {
-            "%{wks.location}/../01.HelloWorld",
-        }
+        includedirs { "%{wks.location}/../01.HelloWorld", }
 
         -- 소스 파일을 추가합니다.
-        files {
-            "%{wks.location}/../01.HelloWorld/*",
-        }
+        files { "%{wks.location}/../01.HelloWorld/*", }
 
         -- 빌드의 구성 요소별 설정을 수행합니다.
         filter "configurations:Debug"
