1e042351fa8ee91b5d16df2116b63032b67e3b0f
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-03-06 09:31:25 +0900

    Chapter05 실습 코드 추가

---
+++ Chapter05/premake5.lua
@@ -0,0 +1,116 @@
+solution "CSharpProgramming"
+    -- 빌드 구성 요소입니다.
+    configurations { "Debug", "Release" }
+
+    -- 빌드 플랫폼입니다.
+    platforms { "Win64" }
+
+    -- 솔루션 경로를 설정합니다.
+    location "%{wks.name}"
+
+    project "01.IfElse"
+        -- 프로젝트의 종류를 설정합니다.
+        kind "ConsoleApp"
+
+        -- 프로그래밍 언어를 설정합니다.
+        language "C#"
+
+        -- 소스 코드 경로를 추가합니다.
+        includedirs { "%{wks.location}/../01.IfElse", }
+
+        -- 소스 파일을 추가합니다.
+        files { "%{wks.location}/../01.IfElse/*", }
+
+        -- 빌드의 구성 요소별 설정을 수행합니다.
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            runtime  "Debug"
+            optimize "Off"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG"  }
+            runtime "Release"
+            optimize "Full"
+            symbols "Off"
+
+
+    project "02.IfIf"
+        -- 프로젝트의 종류를 설정합니다.
+        kind "ConsoleApp"
+
+        -- 프로그래밍 언어를 설정합니다.
+        language "C#"
+
+        -- 소스 코드 경로를 추가합니다.
+        includedirs { "%{wks.location}/../02.IfIf", }
+
+        -- 소스 파일을 추가합니다.
+        files { "%{wks.location}/../02.IfIf/*", }
+
+        -- 빌드의 구성 요소별 설정을 수행합니다.
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            runtime  "Debug"
+            optimize "Off"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG"  }
+            runtime "Release"
+            optimize "Full"
+            symbols "Off"
+
+
+    project "03.Switch"
+        -- 프로젝트의 종류를 설정합니다.
+        kind "ConsoleApp"
+
+        -- 프로그래밍 언어를 설정합니다.
+        language "C#"
+
+        -- 소스 코드 경로를 추가합니다.
+        includedirs { "%{wks.location}/../03.Switch", }
+
+        -- 소스 파일을 추가합니다.
+        files { "%{wks.location}/../03.Switch/*", }
+
+        -- 빌드의 구성 요소별 설정을 수행합니다.
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            runtime  "Debug"
+            optimize "Off"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG"  }
+            runtime "Release"
+            optimize "Full"
+            symbols "Off"
+
+
+    project "04.Switch2"
+        -- 프로젝트의 종류를 설정합니다.
+        kind "ConsoleApp"
+
+        -- 프로그래밍 언어를 설정합니다.
+        language "C#"
+
+        -- 소스 코드 경로를 추가합니다.
+        includedirs { "%{wks.location}/../04.Switch2", }
+
+        -- 소스 파일을 추가합니다.
+        files { "%{wks.location}/../04.Switch2/*", }
+
+        -- 빌드의 구성 요소별 설정을 수행합니다.
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            runtime  "Debug"
+            optimize "Off"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG"  }
+            runtime "Release"
+            optimize "Full"
+            symbols "Off"
