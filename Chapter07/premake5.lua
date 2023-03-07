7d8620a39ac1e5aee71b3ffeda31718ffdd590f1
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-03-08 08:20:06 +0900

    Chapter07 실습 코드 추가

--- Chapter07/premake5.lua
+++ Chapter07/premake5.lua
@@ -96,3 +96,183 @@ solution "CSharpProgramming"
             runtime "Release"
             optimize "Full"
             symbols "Off"
+
+
+    project "04.DeepCopy"
+        -- 프로젝트의 종류를 설정합니다.
+        kind "ConsoleApp"
+
+        -- 프로그래밍 언어를 설정합니다.
+        language "C#"
+
+        -- C# 버전을 8.0으로 설정합니다.
+        csversion("8.0")
+
+        -- 소스 코드 경로를 추가합니다.
+        includedirs { "%{wks.location}/../04.DeepCopy", }
+
+        -- 소스 파일을 추가합니다.
+        files { "%{wks.location}/../04.DeepCopy/*", }
+
+        -- 빌드의 구성 요소별 설정을 수행합니다.
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            runtime  "Debug"
+            optimize "Off"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG"  }
+            runtime "Release"
+            optimize "Full"
+            symbols "Off"
+
+
+    project "05.This"
+        -- 프로젝트의 종류를 설정합니다.
+        kind "ConsoleApp"
+
+        -- 프로그래밍 언어를 설정합니다.
+        language "C#"
+
+        -- C# 버전을 8.0으로 설정합니다.
+        csversion("8.0")
+
+        -- 소스 코드 경로를 추가합니다.
+        includedirs { "%{wks.location}/../05.This", }
+
+        -- 소스 파일을 추가합니다.
+        files { "%{wks.location}/../05.This/*", }
+
+        -- 빌드의 구성 요소별 설정을 수행합니다.
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            runtime  "Debug"
+            optimize "Off"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG"  }
+            runtime "Release"
+            optimize "Full"
+            symbols "Off"
+
+
+    project "06.ThisConstructor"
+        -- 프로젝트의 종류를 설정합니다.
+        kind "ConsoleApp"
+
+        -- 프로그래밍 언어를 설정합니다.
+        language "C#"
+
+        -- C# 버전을 8.0으로 설정합니다.
+        csversion("8.0")
+
+        -- 소스 코드 경로를 추가합니다.
+        includedirs { "%{wks.location}/../06.ThisConstructor", }
+
+        -- 소스 파일을 추가합니다.
+        files { "%{wks.location}/../06.ThisConstructor/*", }
+
+        -- 빌드의 구성 요소별 설정을 수행합니다.
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            runtime  "Debug"
+            optimize "Off"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG"  }
+            runtime "Release"
+            optimize "Full"
+            symbols "Off"
+
+
+    project "07.AccessModifier"
+        -- 프로젝트의 종류를 설정합니다.
+        kind "ConsoleApp"
+
+        -- 프로그래밍 언어를 설정합니다.
+        language "C#"
+
+        -- C# 버전을 8.0으로 설정합니다.
+        csversion("8.0")
+
+        -- 소스 코드 경로를 추가합니다.
+        includedirs { "%{wks.location}/../07.AccessModifier", }
+
+        -- 소스 파일을 추가합니다.
+        files { "%{wks.location}/../07.AccessModifier/*", }
+
+        -- 빌드의 구성 요소별 설정을 수행합니다.
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            runtime  "Debug"
+            optimize "Off"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG"  }
+            runtime "Release"
+            optimize "Full"
+            symbols "Off"
+
+
+    project "08.Inheritance"
+        -- 프로젝트의 종류를 설정합니다.
+        kind "ConsoleApp"
+
+        -- 프로그래밍 언어를 설정합니다.
+        language "C#"
+
+        -- C# 버전을 8.0으로 설정합니다.
+        csversion("8.0")
+
+        -- 소스 코드 경로를 추가합니다.
+        includedirs { "%{wks.location}/../08.Inheritance", }
+
+        -- 소스 파일을 추가합니다.
+        files { "%{wks.location}/../08.Inheritance/*", }
+
+        -- 빌드의 구성 요소별 설정을 수행합니다.
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            runtime  "Debug"
+            optimize "Off"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG"  }
+            runtime "Release"
+            optimize "Full"
+            symbols "Off"
+
+
+    project "09.TypeCasting"
+        -- 프로젝트의 종류를 설정합니다.
+        kind "ConsoleApp"
+
+        -- 프로그래밍 언어를 설정합니다.
+        language "C#"
+
+        -- C# 버전을 8.0으로 설정합니다.
+        csversion("8.0")
+
+        -- 소스 코드 경로를 추가합니다.
+        includedirs { "%{wks.location}/../09.TypeCasting", }
+
+        -- 소스 파일을 추가합니다.
+        files { "%{wks.location}/../09.TypeCasting/*", }
+
+        -- 빌드의 구성 요소별 설정을 수행합니다.
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            runtime  "Debug"
+            optimize "Off"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG"  }
+            runtime "Release"
+            optimize "Full"
+            symbols "Off"
